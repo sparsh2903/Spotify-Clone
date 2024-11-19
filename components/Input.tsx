d3103aac67e78cc480d44352ccd27223cc046682
Sparsh Tiwari <85448284+sparsh2903@users.noreply.github.com>
2024-11-19 23:27:12 +0530

    Initial commit with project files

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,47 +1,42 @@
 import { forwardRef } from "react";
-import { GiFlexibleLamp } from "react-icons/gi";
 import { twMerge } from "tailwind-merge";
 
-interface InputProps
- extends React.InputHTMLAttributes<HTMLInputElement> {}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({
-className,
-type,
-disabled,
-...props
-}, ref) => {
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, disabled, ...props }, ref) => {
     return (
-        <input 
+      <input
         type={type}
-        className={twMerge(`
-           flex
-           w-full
-           rounded-md
-           bg-neutral-700
-           border
-           border-transparent
-           px-3
-           py-3
-           text-sm
-           file:border-0
-           file:bg-transparent
-           file:text-sm
-           file:font-medium
-           placeholder:text-neutral-400
-           disabled:cursor-not-allowed
-           disabled:opacity-50
-           focus:outline-none  
-        `,
-        className
-    )}
-    disabled={disabled}
-    ref={ref}
-    {...props}
-    
-        />
-    )
-});
+        className={twMerge(
+          `
+            flex
+            w-full
+            rounded-md
+            bg-neutral-700
+            border
+            border-transparent
+            px-3
+            py-3
+            text-sm
+            file:border-0
+            file:bg-transparent
+            file:text-sm
+            file:font-medium
+            placeholder:text-neutral-400
+            disabled:cursor-not-allowed
+            disabled:opacity-50
+            focus:outline-none  
+          `,
+          className
+        )}
+        disabled={disabled}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 
 Input.displayName = "Input";
 
